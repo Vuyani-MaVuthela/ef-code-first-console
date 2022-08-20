b0a896346c37efa0b48dbe52f8ce2044befc4d8a
Vuyani Moyo (MEA) <vuyani.moyo@dimensiondata.com>
2022-08-20 18:45:33 +0200

    first commit

---
+++ ef-code-first-console/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace ef_code_first_console.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<ef_code_first_console.PostsDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(ef_code_first_console.PostsDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
---
+++ ef-code-first-console/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ef_code_first_console
+{
+    public class Post
+    {
+        public int PostID { get; set; }
+        public System.DateTime DatePublished { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+    }
+
+    public class PostsDbContext : DbContext //database api
+    {
+        public DbSet<Post> Posts { get; set; } //database table
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
